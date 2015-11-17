545339c65367a9376755c31349b8723e781a09f8
Luiz Romário Santana Rios <luizromario@gmail.com>
2015-11-17 20:33:30 -0300

    Add script file to merge all db files

---
+++ merge_dbs.rb
@@ -0,0 +1,22 @@
+IO.write(
+  "leak_summary.txt",
+  Dir.glob("#{ARGV[0]}/*").map do |f|
+    IO.read(f).split("\n").map do |e|
+      s = e.strip.split
+      puts "Null password in #{f}! Count: #{s[0]}" if s[1].nil?
+    
+      e.strip
+    end
+  end.flatten.map do |e|
+    s = e.split
+
+    [s[0].to_i, s[1]]
+  end.group_by{|e| e[1] }.map do |k, g|
+    [
+      k,
+      g.map{|e| e[0] }.reduce(&:+)
+    ]
+  end.sort_by{|e| e[1] }.reverse.reduce("") do |str, e|
+    str += "#{e[1]}\t#{e[0]}\n"
+  end
+)

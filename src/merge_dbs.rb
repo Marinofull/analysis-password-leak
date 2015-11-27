bb16edc78712d32f162f33d3ccd1c14edcd9b319
Marinofull <intmarinoreturn0@gmail.com>
2015-11-26 23:26:10 -0300

    table of password structure #5

--- src/merge_dbs.rb
+++ src/merge_dbs.rb
@@ -166,15 +166,15 @@ def password_structure(pass)
 end
 
 def structure_analysis(passwords)
-  any_digits = 0
-  only_lowercase_letter = 0
-  lowercase_and_digits = 0
-  lowercase_and_symbols = 0
-  digit_and_symbols = 0
-  symbols = 0
-  lowercase_digit_symbols = 0
-  bad_words = 0
-  samerow = 0
+  any_digits = 0.0
+  only_lowercase_letter = 0.0
+  lowercase_and_digits = 0.0
+  lowercase_and_symbols = 0.0
+  digit_and_symbols = 0.0
+  symbols = 0.0
+  lowercase_digit_symbols = 0.0
+  bad_words = 0.0
+  samerow = 0.0
   
   structures = {}
   structures.default_proc = proc { 0 }
@@ -198,19 +198,19 @@ def structure_analysis(passwords)
   digit_and_symbols -= ( symbols + any_digits )
   
   structures_text = structures.sort_by{|e| e[1] }.reverse.reduce("\n") do |str, e|
-    str += "      #{e[0]}\t\t#{e[1]}\n"
+    str += "      #{e[0]}\t\t#{e[1]}\t#{'%.3f' % (e[1].to_f/passwords.total * 100)}%\n"
   end
 
   <<EOF
-    #{any_digits} any digits
-    #{only_lowercase_letter} only lowercase letters
-    #{lowercase_and_digits} lowercase and digits
-    #{lowercase_and_symbols} lowercase and symbols
-    #{digit_and_symbols} digit and symbols
-    #{symbols} symbols
-    #{lowercase_digit_symbols} lowercase digit symbols
-    #{bad_words} contain bad words
-    #{samerow} with characters in the same row
+    #{any_digits} any digits #{'%.3f' % (any_digits/passwords.total * 100)}
+    #{only_lowercase_letter} only lowercase letters #{'%.3f' % (only_lowercase_letter/passwords.total * 100)} 
+    #{lowercase_and_digits} lowercase and digits #{'%.3f' % (lowercase_and_digits/passwords.total * 100)}
+    #{lowercase_and_symbols} lowercase and symbols #{'%.3f' % (lowercase_and_symbols/passwords.total * 100)} 
+    #{digit_and_symbols} digit and symbols #{'%.3f' % (digit_and_symbols/passwords.total * 100)}
+    #{symbols} symbols #{'%.3f' % (symbols/passwords.total * 100)}
+    #{lowercase_digit_symbols} lowercase digit symbols #{'%.3f' % (lowercase_digit_symbols/passwords.total * 100)} 
+    #{bad_words} contain bad words #{'%.3f' % (bad_words/passwords.total * 100)}
+    #{samerow} with characters in the same row #{'%.3f' % (samerow/passwords.total * 100)}
     ==================================================
     Structures:#{structures_text}
 EOF

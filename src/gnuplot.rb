0147f95486b2c6d976774b1a789f2e4d3cb3eda7
Marinofull <intmarinoreturn0@gmail.com>
2015-11-27 13:41:00 -0300

    Revisto paragrafos incopletos, e considerações finais
    And also It Closes #25

--- src/gnuplot.rb
+++ src/gnuplot.rb
@@ -18,3 +18,19 @@ def plot_hist(dat, output, xlabel, ylabel, title = "logscale histogram")
   gnuplot(commands)
 end
 
+def plot_badwords(dat, output, xlabel, ylabel, title = "Eschatological Relevance Password")
+  commands = %Q(
+    set title '#{title}'
+    set key right bottom
+    set xlabel "#{xlabel}"
+    set ylabel "#{ylabel}"
+    
+    set term png size 1000,500
+    set output "#{output}"
+    set logscale y
+    set xtics 100
+    plot "#{dat}" using 2: xtic(1) with histogram notitle
+  )
+  gnuplot(commands)
+end
+
